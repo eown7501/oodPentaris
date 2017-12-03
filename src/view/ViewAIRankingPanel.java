8140fc2c1d7c915c8c50e27172941bef10f93451
DESKTOP-CKD2MIS\이은경 <e_gyeong@naver.com>
2017-12-03 19:38:35 +0900

    Solo/AI RankingPanel 수정

--- src/view/ViewAIRankingPanel.java
+++ src/view/ViewAIRankingPanel.java
@@ -1,113 +1,232 @@
 package view;
 
-import javax.swing.*;
-import ranking.RankingData;
 import ranking.RankingManager;
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Image;
+import java.awt.GridLayout;
 import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import javax.swing.*;
 
 /**
  * 이 ViewAIRankingPanel 클래스는 AIRanking화면의 Panel을 구성하고 보여주는 클래스입니다.
+ * 
  * @author 이은경
  */
 public class ViewAIRankingPanel extends JPanel {
 
-	/** ViewTotalFrame Type의 변수 입니다. */
+	/** ViewTotalFrame Type 의 변수 입니다. */
 	private ViewTotalFrame totalFrame;
-	/** JLabel Type의 변수 입니다. */
-	private JLabel viewAIRanking;
-	/** JButton Type의 변수 입니다. */
-	private JButton backbt;
-	/** images에 저장된 이미지 사용할 변수 입니다. */
-	private ImageIcon image = new ImageIcon("images/black.png");
-	/** image를 반환할 변수 입니다. */
-	private Image im = image.getImage();
-	/** RankingManager Type의 변수입니다. */
+	/** RankingManager Type 의 변수 입니다. */
 	private RankingManager rankingManager;
-	/** RankingData[] Type 의 변수입니다. */
-	private RankingData[] AIRankingData;
+	/** JPanel Type 의 변수 입니다. */
+	private JPanel panel1, panel2, panel3;
+	/** JLabel Type 의 변수 입니다. */
+	private JLabel nameLabel;
+	/** JButton Type 의 변수 입니다. */
+	private JButton resetbt, backbt;
+	/** FileReader Type 의 변수입니다. */
+	private FileReader fr;
+	/** BufferedReader Type 의 변수입니다. */
+	private BufferedReader br;
+	/** String Type 의 변수입니다. */
+	private String line, subStr, subStr2, content;
+	/** JLabel[] Type 의 변수입니다. */
+	private JLabel[] listLabel;
+	/** StringTokenizer Type 의 변수입니다. */
+	private StringTokenizer token, token2;
 
 	/**
-	 * ViewAIRankingPanel을 생성합니다.
-	 * @param totalFrame - 명령을 전달받을 totalFrame 입니다.
+	 *  ViewAIRankingpanel을 생성합니다. 
+	 * @param totalFrame - 명령을 전달 받을 totalFrame 입니다.
 	 */
 	public ViewAIRankingPanel(ViewTotalFrame totalFrame) {
 		this.totalFrame = totalFrame;
-		viewAIRanking = new JLabel("");
-		backbt = new JButton("back");
 		init();
 	}
-
-	/**
-	 * ViewAIRankingPanel을 생성합니다.
-	 * @param AIRankingData - 명령을 전달받을 AIRankingData 입니다.
-	 */
-	public ViewAIRankingPanel(RankingData[] AIRankingData) {
-		this.AIRankingData = AIRankingData;
-	}
-
-	/** Panel을 초기화 합니다. */
+	
+	/** 초기값 설정을 합니다. */
 	public void init() {
-		addLabel();
-		addButton();
+		rankingManager = new RankingManager();
+		setBackground(Color.BLACK);
+		setLayout(null);
+		addContents();
 		addListener();
-
+		setVisible(true);
 	}
 
+	/** Contents의 기본 설정을 세팅합니다. */
+	public void addContents(){
+		panel1 = new JPanel();
+		panel2 = new JPanel();
+		panel3 = new JPanel();
+		resetbt = new JButton("reset");
+		backbt = new JButton("back");
+		nameLabel = new JLabel("AIGame Ranking");
+		listLabel = new JLabel[10];
+		
+		for(int i = 0 ; i < 10 ; i++) {
+			listLabel[i] = new JLabel();
+			listLabel[i].setFont(new Font("Forte", Font.PLAIN, 30));
+			listLabel[i].setBackground(Color.BLACK);
+			listLabel[i].setForeground(Color.WHITE);
+			panel2.add(listLabel[i]);
+		}
+		
+		nameLabel.setFont(new Font("Forte", Font.PLAIN, 45));
+		nameLabel.setForeground(Color.WHITE);
+		resetbt.setFont(new Font("Forte", Font.PLAIN, 45));
+		backbt.setFont(new Font("Forte", Font.PLAIN, 45));
+		
+		resetbt.setBounds(297, 380, 450, 60);
+		resetbt.setBorderPainted(false);
+		resetbt.setForeground(Color.WHITE);
+		resetbt.setFocusable(false);
+		resetbt.setContentAreaFilled(false);
+		
+		backbt.setBounds(800, 380, 450, 60);
+		backbt.setBorderPainted(false);
+		backbt.setForeground(Color.WHITE);
+		backbt.setFocusable(false);
+		backbt.setContentAreaFilled(false);
+		
+		printRanking();
+		
+		panel2.setLayout(new GridLayout(10, 1, 5, 5));
+
+		panel1.setBounds(0, 0, 1050, 100);
+		panel2.setBounds(200, 110, 650, 450);
+		panel3.setBounds(0, 550, 1050, 100);
+		
+		panel1.setBackground(Color.BLACK);
+		panel2.setBackground(Color.BLACK);
+		panel3.setBackground(Color.BLACK);
+
+		panel1.add(nameLabel, JLabel.CENTER);
+		panel3.add(resetbt);
+		panel3.add(Box.createHorizontalStrut(50));
+		panel3.add(backbt);
+		
+		this.add(panel1);
+		this.add(panel2);
+		this.add(panel3);
+	}
+	
 	/** 스윙 컴포넌트가 자신의 모양을 그립니다. */
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.drawImage(im, 0, 0, getWidth(), getHeight(), this);
+		resetListLabel();
+		printRanking();
+	}
+	
+	/** AIRanking을 나타내는 Label을 초기화합니다. */
+	public void resetListLabel() {
+		for (int i = 0; i < 10 ; i++) {
+			listLabel[i].setText("");
+		}
+	}
+	
+	/** AIRanking을 출력합니다. */ 
+	public void printRanking(){
+		try {
+			getAITXT();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/** 텍스트 파일로부터 값을 읽어옵니다. */
+	public void getAITXT() throws FileNotFoundException{
+		fr = new FileReader("AIRanking.txt");
+		br = new BufferedReader(fr);
+		token = null;
+		int num = 0;
+		try {
+			while((line = br.readLine()) != null) {
+				if(!line.equals("")) {
+					token = new StringTokenizer(line, "\n");
+					
+					while(token.hasMoreTokens()) {
+						subStr = token.nextToken();
+						token2 = new StringTokenizer(subStr, " ");
+						content = "";
+						
+						while(token2.hasMoreTokens()){
+							subStr2 = token2.nextToken();	
+							content += subStr2 + " ";
+						}
+						listLabel[num++].setText(content);
+					}
+				}
+			}
+			panel2.repaint();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
-	/** Label을 추가합니다. */
-	public void addLabel() {
-		this.add(viewAIRanking);
-	}
+	
 
-	/** Button을 추가합니다. */
-	public void addButton() {
-		backbt.setFont(new Font("SVN-Block", Font.PLAIN, 16));
-		this.add(backbt);
-	}
-
-	/** Button에 MouseListener를 추가합니다. */
+	/** Button에 MouseListener를 삽입합니다. */
 	public void addListener() {
-		backbt.addMouseListener(makeMouseListener());
-
+		backbt.addMouseListener(makeListener());
+		resetbt.addMouseListener(makeListener());
+		
 	}
 
-	/**
-	 * MouseListener을 만듭니다.
-	 * @return 이 MouseListener를 반환합니다.
+	/** MouseLister를 구현합니다. 
+	 * @return MouseEvent를 반환합니다. 
 	 */
-	public MouseListener makeMouseListener() {
+	public MouseListener makeListener() {
 		return new MouseAdapter() {
 			public void mouseEntered(MouseEvent e) {
-				if (e.getSource() == backbt) {
-					backbt.setFont(new Font("SVN-Block", Font.PLAIN, 20));
+				if(e.getSource() == resetbt){
+					resetbt.setFont(new Font("Forte", Font.PLAIN, 55));
+				}
+				if(e.getSource() == backbt){
+					backbt.setFont(new Font("Forte", Font.PLAIN, 55));
 				}
 			}
-
 			public void mouseExited(MouseEvent e) {
-				if (e.getSource() == backbt) {
-					backbt.setFont(new Font("SVN-Block", Font.PLAIN, 16));
+				if(e.getSource() == resetbt){
+					resetbt.setFont(new Font("Forte", Font.PLAIN, 45));
+				}
+				if(e.getSource() == backbt){
+					backbt.setFont(new Font("Forte", Font.PLAIN, 45));
 				}
 			}
-
 			public void mouseClicked(MouseEvent e) {
+				if (e.getSource() == resetbt) {
+					try {
+						rankingManager.resetAIData();
+						showAIRankingResetPanel();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
 				if (e.getSource() == backbt) {
-					ViewRankingPanel();
+					showRankingPanel();
 				}
 			}
 		};
 	}
 
 	/** RankingPanel을 보여줍니다. */
-	public void ViewRankingPanel() {
+	public void showRankingPanel() {
 		totalFrame.showRankingPanel();
-
 	}
+	
+	/** AIRankingResetPanel을 보여줍니다. */
+	public void showAIRankingResetPanel() {
+		totalFrame.showAIRankingResetPanel();
+	}
+	
 }
--- src/view/ViewSoloRankingPanel.java
+++ src/view/ViewSoloRankingPanel.java
@@ -1,122 +1,184 @@
 package view;
 
-import ranking.RankingData;
 import ranking.RankingManager;
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Image;
+import java.awt.GridLayout;
 import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.StringTokenizer;
 import javax.swing.*;
 
 /**
  * 이 ViewSoloRankingPanel 클래스는 SoloRanking화면의 Panel을 구성하고 보여주는 클래스입니다.
+ * 
  * @author 이은경
  */
 public class ViewSoloRankingPanel extends JPanel {
 
-	/** ViewTotalFrame Type의 변수 입니다. */
+	/** ViewTotalFrame Type 의 변수 입니다. */
 	private ViewTotalFrame totalFrame;
-	/** RankingManager Type의 변수 입니다. */
+	/** RankingManager Type 의 변수 입니다. */
 	private RankingManager rankingManager;
-	/** RankingData[] Type의 변수 입니다. */
-	private RankingData[] soloRankingData;
-	/** JPanel Type의 변수 입니다. */
-	private JPanel panel1, panel2, panel3, secFrmPanel1, secFrmPanel2;
-	/** JLabel Type의 변수 입니다. */
-	private JLabel namelabel, secFrmLabel;
-	/** JTextArea Type의 변수 입니다. */
-	private JTextArea list;
-	/** JButton Type의 변수 입니다. */
-	private JButton resetbt, backbt, yesbt, nobt;
-	/** images에 저장된 이미지 사용할 변수 입니다. */
-	private ImageIcon image = new ImageIcon("images/black.png");
-	/** image를 반환할 변수 입니다. */
-	private Image im = image.getImage();
+	/** JPanel Type 의 변수 입니다. */
+	private JPanel panel1, panel2, panel3;
+	/** JLabel Type 의 변수 입니다. */
+	private JLabel nameLabel;
+	/** JButton Type 의 변수 입니다. */
+	private JButton resetbt, backbt;
+	/** FileReader Type 의 변수입니다. */
+	private FileReader fr;
+	/** BufferedReader Type 의 변수입니다. */
+	private BufferedReader br;
+	/** String Type 의 변수입니다. */
+	private String line, subStr, subStr2, content;
+	/** JLabel[] Type 의 변수입니다. */
+	private JLabel[] listLabel;
+	/** StringTokenizer Type 의 변수입니다. */
+	private StringTokenizer token, token2;
 
 	/**
 	 *  ViewSoloRankingpanel을 생성합니다. 
 	 * @param totalFrame - 명령을 전달 받을 totalFrame 입니다.
 	 */
 	public ViewSoloRankingPanel(ViewTotalFrame totalFrame) {
-
 		this.totalFrame = totalFrame;
-		panel1 = new JPanel();
-		panel2 = new JPanel();
-		panel3 = new JPanel();
-		namelabel = new JLabel("score랭킹");
-		list = new JTextArea(5, 40); // txt파일을 읽어와야 함
-		resetbt = new JButton("reset");
-		backbt = new JButton("back");
-		yesbt = new JButton("Yes");
-		nobt = new JButton("No");
-		rankingManager = new RankingManager();
-		soloRankingData = new RankingData[10];
-
-		init1();
+		init();
 	}
-
-	/**
-	 * ViewSoloRankingPanel을 생성합니다.
-	 * @param soloRankingData - 명령을 전달받을 soloRankingData 입니다.
-	 */
-	public ViewSoloRankingPanel(RankingData[] soloRankingData) {
-		this.soloRankingData = soloRankingData;
-		init2();
-	}
-
+	
 	/** 초기값 설정을 합니다. */
-	public void init1() {
-		setLayout(new BorderLayout());
+	public void init() {
+		rankingManager = new RankingManager();
+		setBackground(Color.BLACK);
+		setLayout(null);
 		addContents();
 		addListener();
 		setVisible(true);
 	}
 
-	/** 랭킹 데이터를 나타냅니다. */
-	public void init2() {
-		// 랭킹데이터를 나타내야 함
-	}
-
-	/**
-	 *  스윙 컴포넌트가 자신의 모양을 그립니다.
-	 */
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		g.drawImage(im, 0, 0, getWidth(), getHeight(), this);
-	}
-
 	/** Contents의 기본 설정을 세팅합니다. */
-	public void addContents() {
-		namelabel.setFont(new Font("SVN-Block", Font.PLAIN, 40));
-		namelabel.setForeground(Color.WHITE);
-		resetbt.setFont(new Font("SVN-Block", Font.PLAIN, 16));
-		backbt.setFont(new Font("SVN-Block", Font.PLAIN, 16));
-		list.setRows(300);
+	public void addContents(){
+		panel1 = new JPanel();
+		panel2 = new JPanel();
+		panel3 = new JPanel();
+		resetbt = new JButton("reset");
+		backbt = new JButton("back");
+		nameLabel = new JLabel("SoloGame Ranking");
+		listLabel = new JLabel[10];
+		
+		for(int i = 0 ; i < 10 ; i++) {
+			listLabel[i] = new JLabel();
+			listLabel[i].setFont(new Font("Forte", Font.PLAIN, 30));
+			listLabel[i].setBackground(Color.BLACK);
+			listLabel[i].setForeground(Color.WHITE);
+			panel2.add(listLabel[i]);
+		}
+		
+		nameLabel.setFont(new Font("Forte", Font.PLAIN, 45));
+		nameLabel.setForeground(Color.WHITE);
+		resetbt.setFont(new Font("Forte", Font.PLAIN, 45));
+		backbt.setFont(new Font("Forte", Font.PLAIN, 45));
+		
+		resetbt.setBounds(297, 380, 450, 60);
+		resetbt.setBorderPainted(false);
+		resetbt.setForeground(Color.WHITE);
+		resetbt.setFocusable(false);
+		resetbt.setContentAreaFilled(false);
+		
+		backbt.setBounds(800, 380, 450, 60);
+		backbt.setBorderPainted(false);
+		backbt.setForeground(Color.WHITE);
+		backbt.setFocusable(false);
+		backbt.setContentAreaFilled(false);
+		
+		printRanking();
+		
+		panel2.setLayout(new GridLayout(10, 1, 5, 5));
 
-		panel1.setBackground(Color.black);
-		panel2.setBackground(Color.black);
-		panel3.setBackground(Color.black);
+		panel1.setBounds(0, 0, 1050, 100);
+		panel2.setBounds(200, 110, 650, 450);
+		panel3.setBounds(0, 550, 1050, 100);
+		
+		panel1.setBackground(Color.BLACK);
+		panel2.setBackground(Color.BLACK);
+		panel3.setBackground(Color.BLACK);
 
-		panel1.add(namelabel, JLabel.CENTER);
-		panel2.add(new JScrollPane(list));
+		panel1.add(nameLabel, JLabel.CENTER);
 		panel3.add(resetbt);
 		panel3.add(Box.createHorizontalStrut(50));
 		panel3.add(backbt);
-
-		this.add(panel1, BorderLayout.NORTH);
-		this.add(panel2, BorderLayout.CENTER);
-		this.add(panel3, BorderLayout.SOUTH);
+		
+		this.add(panel1);
+		this.add(panel2);
+		this.add(panel3);
 	}
+	
+	/** 스윙 컴포넌트가 자신의 모양을 그립니다. */
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		resetListLabel();
+		printRanking();
+	}
+	
+	/** SoloRanking을 나타내는 Label을 초기화합니다. */
+	public void resetListLabel() {
+		for (int i = 0; i < 10 ; i++) {
+			listLabel[i].setText("");
+		}
+	}
+	
+	/** SoloRanking을 출력합니다. */ 
+	public void printRanking(){
+		try {
+			getSoloTXT();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/** 텍스트 파일로부터 값을 읽어옵니다. */
+	public void getSoloTXT() throws FileNotFoundException{
+		fr = new FileReader("SoloRanking.txt");
+		br = new BufferedReader(fr);
+		token = null;
+		int num = 0;
+		try {
+			while((line = br.readLine()) != null) {
+				if(!line.equals("")) {
+					token = new StringTokenizer(line, "\n");
+					
+					while(token.hasMoreTokens()) {
+						subStr = token.nextToken();
+						token2 = new StringTokenizer(subStr, " ");
+						content = "";
+						
+						while(token2.hasMoreTokens()){
+							subStr2 = token2.nextToken();	
+							content += subStr2 + " ";
+						}
+						listLabel[num++].setText(content);
+					}
+				}
+			}
+			panel2.repaint();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	
 
 	/** Button에 MouseListener를 삽입합니다. */
 	public void addListener() {
 		backbt.addMouseListener(makeListener());
 		resetbt.addMouseListener(makeListener());
-		yesbt.addMouseListener(makeListener());
-		nobt.addMouseListener(makeListener());
+		
 	}
 
 	/** MouseLister를 구현합니다. 
@@ -125,38 +187,30 @@ public class ViewSoloRankingPanel extends JPanel {
 	public MouseListener makeListener() {
 		return new MouseAdapter() {
 			public void mouseEntered(MouseEvent e) {
-				if (e.getSource() == resetbt) {
-					resetbt.setFont(new Font("SVN-Block", Font.PLAIN, 20));
+				if(e.getSource() == resetbt){
+					resetbt.setFont(new Font("Forte", Font.PLAIN, 55));
 				}
-				if (e.getSource() == backbt) {
-					backbt.setFont(new Font("SVN-Block", Font.PLAIN, 20));
-				}
-				if (e.getSource() == yesbt) {
-					yesbt.setFont(new Font("SVN-Block", Font.PLAIN, 20));
-				}
-				if (e.getSource() == nobt) {
-					nobt.setFont(new Font("SVN-Block", Font.PLAIN, 20));
+				if(e.getSource() == backbt){
+					backbt.setFont(new Font("Forte", Font.PLAIN, 55));
 				}
 			}
-
 			public void mouseExited(MouseEvent e) {
-				if (e.getSource() == resetbt) {
-					resetbt.setFont(new Font("SVN-Block", Font.PLAIN, 16));
+				if(e.getSource() == resetbt){
+					resetbt.setFont(new Font("Forte", Font.PLAIN, 45));
 				}
-				if (e.getSource() == backbt) {
-					backbt.setFont(new Font("SVN-Block", Font.PLAIN, 16));
-				}
-				if (e.getSource() == yesbt) {
-					yesbt.setFont(new Font("SVN-Block", Font.PLAIN, 16));
-				}
-				if (e.getSource() == nobt) {
-					nobt.setFont(new Font("SVN-Block", Font.PLAIN, 16));
+				if(e.getSource() == backbt){
+					backbt.setFont(new Font("Forte", Font.PLAIN, 45));
 				}
 			}
-
 			public void mouseClicked(MouseEvent e) {
 				if (e.getSource() == resetbt) {
-					new SecFrm();
+					try {
+						rankingManager.resetSoloData();
+						showSoloRankingResetPanel();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
 				}
 				if (e.getSource() == backbt) {
 					showRankingPanel();
@@ -169,57 +223,9 @@ public class ViewSoloRankingPanel extends JPanel {
 	public void showRankingPanel() {
 		totalFrame.showRankingPanel();
 	}
-
-	/** */
-	class SecFrm extends JFrame {
-		SecFrm() {
-			setTitle("경고!");
-			setSize(350, 150);
-			secFrmPanel1 = new JPanel();
-			secFrmPanel2 = new JPanel();
-			secFrmLabel = new JLabel("정말 초기화하시겠습니까?");
-
-			yesbt.addActionListener(makeListener2());
-			nobt.addActionListener(makeListener2());
-
-			secFrmLabel.setFont(new Font("SVN-Block", Font.PLAIN, 16));
-
-			secFrmPanel1.add(secFrmLabel, JLabel.CENTER);
-			secFrmPanel2.add(yesbt);
-			secFrmPanel2.add(Box.createHorizontalStrut(20));
-			secFrmPanel2.add(nobt);
-
-			this.add(secFrmPanel1, BorderLayout.CENTER);
-			this.add(secFrmPanel2, BorderLayout.SOUTH);
-
-			setLocationRelativeTo(null);
-			setVisible(true);
-		}
-
-		/** */
-		public ActionListener makeListener2() {
-			return new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					if (e.getSource() == yesbt) {
-						ok();
-						try {
-							rankingManager.resetSoloData(soloRankingData);
-						} catch (IOException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-					}
-					if (e.getSource() == nobt) {
-						ok();
-					}
-				}
-			};
-
-		}
-
-		/** */
-		public void ok() {
-			this.dispose();
-		}
+	
+	/** SoloRankingResetPanel을 보여줍니다. */
+	public void showSoloRankingResetPanel() {
+		totalFrame.showSoloRankingResetPanel();
 	}
 }
